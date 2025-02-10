5db1555985cf86ba302872d850d6df16f855fe30
Dan PN <77532827+Derlvx@users.noreply.github.com>
2025-02-10 17:19:57 +0100

    fix: :bug: casse-sensitive

--- src/Components/Canvas/Canvas.jsx
+++ src/Components/Canvas/Canvas.jsx
@@ -1,12 +1,12 @@
 import s from "./Canvas.module.scss";
-import scene from "../../webgl/Scene";
+import Scene from "../../webgl/Scene";
 import { useEffect, useRef } from "react";
 
 const Canvas = () => {
   const canvasRef = useRef();
 
   useEffect(() => {
-    scene.setup(canvasRef.current);
+    Scene.setup(canvasRef.current);
   }, []);
 
   return <canvas ref={canvasRef}></canvas>;
