dc4a328796a7dcec03b36ad52439924c28546ce8
Dan PN <77532827+Derlvx@users.noreply.github.com>
2025-02-10 18:21:48 +0100

    fix: :bug: thanks to git change folder components to Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import Landing from '@components/Landing/Landing.jsx'
-import Dropzone from '@components/Dropzone/Dropzone.jsx'
-import Tracks from '@components/Tracks/Tracks.jsx'
-import Picker from '@components/Picker/Picker.jsx'
-import Canvas from '@components/Canvas/Canvas.jsx'
+import Landing from './Components/Landing/Landing.jsx'
+import Dropzone from './Components/Dropzone/Dropzone.jsx'
+import Tracks from './Components/Tracks/Tracks.jsx'
+import Picker from './Components/Picker/Picker.jsx'
+import Canvas from './Components/Canvas/Canvas.jsx'
 
 function App() {
   return (
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import glslify from 'rollup-plugin-glslify'
-import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), glslify()],
-  resolve: {
-    alias: {
-      '@components': path.resolve(__dirname, 'src/components'),
-    },
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-  },
-  build: {
-    outDir: 'dist',  // Ou tout autre dossier où tu veux générer ton build
-    sourcemap: true,  // Activer les sourcemaps pour aider au debugging
-  },
 })
