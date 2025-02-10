ee6606d09381f99f488a76ea562a3157af1cab94
Dan PN <77532827+Derlvx@users.noreply.github.com>
2025-02-10 18:14:50 +0100

    fix: :bug: try to change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import glslify from 'rollup-plugin-glslify'
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), glslify()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+    },
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  build: {
+    outDir: 'dist',  // Ou tout autre dossier où tu veux générer ton build
+    sourcemap: true,  // Activer les sourcemaps pour aider au debugging
+  },
 })
